cebaafb6b5314ffe741ed21ab4beb291a39ebbfa
zenoworld <subhromajumder52@gmail.com>
2025-08-24 20:22:03 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
+import { AuthContextProvider } from './context/Context.jsx'
 import './index.css'
 import App from './App.jsx'
-import { AuthContextProvider } from './context/Context.jsx'
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthContextProvider>
-      <BrowserRouter
+      <HashRouter
         future={{
           v7_startTransition: true,
           v7_relativeSplatPath: true,
         }}
       >
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </AuthContextProvider>
   </StrictMode>
 )
